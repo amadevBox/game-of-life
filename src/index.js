5903fe4d47ef1a448bf9129c5a1d0b5367dd80e0
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2018-03-18 03:55:35 +0200

    add generation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const fieldSize = 500
-
 const numberOfCellsInRow = 20
+const framesPerSecond = 5
 
 const getGrid = () => {
   const grid = new Array(numberOfCellsInRow)
@@ -11,6 +11,41 @@ const getGrid = () => {
     }
   }
   return grid
+}
+
+const getNextGeneration = (grid) => {
+  const nextGrid = new Array(grid.length)
+  for (let i = 0; i < grid.length; i++) {
+    nextGrid[i] = new Array(grid.length)
+    for (let j = 0; j < nextGrid[i].length; j++) {
+      const value = grid[i][j]
+      const neighbors = countNeighbors(grid, i, j)
+      if (value === 0 && neighbors === 3) {
+        nextGrid[i][j] = 1
+      } else if (
+        (value === 1) &&
+        (neighbors < 2 || neighbors > 3)
+      ) {
+        nextGrid[i][j] = 0
+      } else {
+        nextGrid[i][j] = value
+      }
+    }
+  }
+  return nextGrid
+}
+
+const countNeighbors = (grid, x, y) => {
+  let sum = 0
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      const col = (x + i + grid.length) % grid.length
+      const row = (y + j + grid.length) % grid.length
+      sum += grid[col][row]
+    }
+  }
+  sum -= grid[x][y]
+  return sum
 }
 
 const cellStrokeColor = '#aaa'
@@ -39,6 +74,16 @@ const drawGrid = (ctx, grid) => {
   }
 }
 
+const generation = (ctx, grid) => {
+  ctx.clearRect(0, 0, fieldSize, fieldSize)
+  drawGrid(ctx, grid)
+  const gridOfNextGeneration = getNextGeneration(grid)
+  setTimeout(() => {
+    requestAnimationFrame(() => generation(ctx, gridOfNextGeneration))
+  }, 1000 / framesPerSecond)
+
+}
+
 window.onload = () => {
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
@@ -46,4 +91,5 @@ window.onload = () => {
   const grid = getGrid()
   console.log(grid)
   drawGrid(ctx, grid)
+  generation(ctx, grid)
 }
